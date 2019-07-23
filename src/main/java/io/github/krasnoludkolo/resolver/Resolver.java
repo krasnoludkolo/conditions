7251bc73961fb6b51a02a35c2f3a6d0ad5ca6f2c
Danek <janek@projmen.pl>
2019-07-23 15:17:29 +0200

    Add and and or

--- src/main/java/io/github/krasnoludkolo/resolver/Resolver.java
+++ src/main/java/io/github/krasnoludkolo/resolver/Resolver.java
@@ -1,8 +1,6 @@
 package io.github.krasnoludkolo.resolver;
 
-import io.vavr.collection.Array;
 import io.vavr.collection.List;
-import io.vavr.collection.Seq;
 import io.vavr.collection.Traversable;
 import io.vavr.control.Either;
 
@@ -16,34 +14,80 @@ public final class Resolver {
     }
 
     @SafeVarargs
-    public static <E> ResolverBuilder<E> when(Condition<E>... conditions) {
-        return when(List.of(conditions));
+    public static <E> LogicalGroup<E> or(LogicalGroup<E>... logicalGroups){
+        return new Or<>(List.of(logicalGroups));
     }
 
-    private static <E> ResolverBuilder<E> when(List<Condition<E>> conditions) {
-        return new ResolverBuilder<>(conditions);
+    @SafeVarargs
+    public static <E> LogicalGroup<E> or(Condition<E>... conditions){
+        List<LogicalGroup<E>> simpleLogicalGroups = List
+                .of(conditions)
+                .map(SimpleCondition::new);
+        return new Or<>(simpleLogicalGroups);
     }
+
+    @SafeVarargs
+    public static <E> LogicalGroup<E> and(Condition<E>... conditions){
+        List<LogicalGroup<E>> simpleLogicalGroups = List
+                .of(conditions)
+                .map(SimpleCondition::new);
+        return new And<>(simpleLogicalGroups);
+    }
+
+    @SafeVarargs
+    public static <E> LogicalGroup<E> and(LogicalGroup<E> logicalGroups, Condition<E>... conditions){
+        return new And<>(List.of(logicalGroups));
+    }
+
+    @SafeVarargs
+    public static <E> LogicalGroup<E> and(LogicalGroup<E>... logicalGroups){
+        return new And<>(List.of(logicalGroups));
+    }
+
+    private static <E> ResolverBuilder<E> when(List<LogicalGroup<E>> logicalGroups) {
+        return new ResolverBuilder<>(logicalGroups);
+    }
+
+    @SafeVarargs
+    public static <E> ResolverBuilder<E> when(Condition<E>... conditions) {
+        return when(or(conditions));
+    }
+
+    @SafeVarargs
+    public static <E> ResolverBuilder<E> when(LogicalGroup<E>... logicalGroups) {
+        return when(List.of(logicalGroups));
+    }
+
+    public static <E> ResolverBuilder<E> when(LogicalGroup<E> logicalGroups) {
+        return when(List.of(logicalGroups));
+    }
+
+    @SafeVarargs
+    public static <E> ResolverBuilder<E> when(LogicalGroup<E> logicalGroups, Condition<E>... conditions) {
+        return when(List.of(logicalGroups));
+    }
+
 
     public static class ResolverBuilder<E> {
 
-        List<Condition<E>> conditions;
-        private ResolverBuilder(List<Condition<E>> conditions) {
-            this.conditions = conditions;
+        List<LogicalGroup<E>> logicalGroups;
+        private ResolverBuilder(List<LogicalGroup<E>> logicalGroup) {
+            this.logicalGroups = logicalGroup;
         }
 
         public <T> Either<E, T> perform(Action<T> action) {
-            return perform(action, conditions);
+            return perform(action, logicalGroups);
         }
 
-        private <T> Either<E, T> perform(Action<T> action, List<Condition<E>> conditions) {
-            return checkConditions(conditions)
+        private <T> Either<E, T> perform(Action<T> action, List<LogicalGroup<E>> logicalGroups) {
+            return checkConditions(logicalGroups)
                     .map(s -> action.perform());
         }
 
-        private Either<E, Success> checkConditions(List<Condition<E>> conditions) {
+        private Either<E, Success> checkConditions(List<LogicalGroup<E>> logicalGroup) {
             return Either.sequence(
-                    conditions
-                            .map(Condition::test)
+                    logicalGroup
+                            .map(LogicalGroup::resolve)
             ).mapLeft(Traversable::head)
                     .map(Success::new);
         }
