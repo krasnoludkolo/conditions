0dc39004ee6008968a8e152af07c7e4b653ae11d
Danek <janek@projmen.pl>
2019-07-24 18:19:55 +0200

    Create handlers as methods

--- src/main/java/io/github/krasnoludkolo/game/GameRestController.java
+++ src/main/java/io/github/krasnoludkolo/game/GameRestController.java
@@ -5,36 +5,41 @@ import io.github.krasnoludkolo.game.api.GameDTO;
 import io.github.krasnoludkolo.infrastructure.http.Controller;
 import io.github.krasnoludkolo.infrastructure.http.JavalinHandler;
 import io.github.krasnoludkolo.infrastructure.http.ResponseResolver;
-import io.javalin.Handler;
+import io.javalin.Context;
 import io.vavr.collection.List;
 
 public final class GameRestController implements Controller {
 
-    private final Handler createGame;
-    private final Handler getGame;
-    private final Handler getAllGames;
+    private final GameFacade gameFacade;
 
-    GameRestController(GameFacade gameFacade){
-        createGame = ctx -> {
-            CreateGameRequestDTO dto = ctx.bodyAsClass(CreateGameRequestDTO.class);
-            gameFacade.createGame(dto.getMaxNumber(), dto.getUserId())
-                    .map(GameDTO::getId)
-                    .peek(id -> ctx.redirect("/games/" + id));
-        };
-        getGame = ctx -> {
-            int id = ctx.pathParam("id", Integer.class).get();
-            ResponseResolver.resolve(gameFacade.getGameById(id), ctx);
-        };
-
-        getAllGames = ctx -> ResponseResolver.resolve(gameFacade.getAllGames(),ctx);
+    GameRestController(GameFacade gameFacade) {
+        this.gameFacade = gameFacade;
     }
 
     @Override
     public List<JavalinHandler> handlers() {
         return List.of(
-                JavalinHandler.post("/games",createGame),
-                JavalinHandler.get("/games",getAllGames),
-                JavalinHandler.get("/games/:id",getGame)
+                JavalinHandler.post("/games", this::createGame),
+                JavalinHandler.get("/games", this::getAllGames),
+                JavalinHandler.get("/games/:id", this::getGame)
         );
     }
+
+    private void createGame(Context ctx) {
+            CreateGameRequestDTO dto = ctx.bodyAsClass(CreateGameRequestDTO.class);
+            gameFacade.createGame(dto.getMaxNumber(), dto.getUserId())
+                    .map(GameDTO::getId)
+                    .peek(id -> ctx.redirect("/games/" + id));
+    }
+
+    private void getAllGames(Context ctx){
+        ResponseResolver
+                .resolve(gameFacade.getAllGames(), ctx);
+    }
+    private void getGame(Context ctx){
+        int id = ctx.pathParam("id", Integer.class).get();
+        ResponseResolver
+                .resolve(gameFacade.getGameById(id), ctx);
+    }
+    
 }
