1027da8ceefb118654e4b2cd83b17943009410db
Danek <janek@projmen.pl>
2019-07-26 16:04:10 +0200

    Refactor controllers

--- src/main/java/io/github/krasnoludkolo/auth/LoginRestController.java
+++ src/main/java/io/github/krasnoludkolo/auth/LoginRestController.java
@@ -5,35 +5,36 @@ import io.github.krasnoludkolo.auth.api.RegisterRequestDTO;
 import io.github.krasnoludkolo.infrastructure.http.Controller;
 import io.github.krasnoludkolo.infrastructure.http.JavalinHandler;
 import io.github.krasnoludkolo.infrastructure.http.ResponseResolver;
-import io.javalin.Handler;
+import io.javalin.Context;
 import io.vavr.collection.List;
 
 public class LoginRestController implements Controller {
 
-    private final Handler login;
-    private final Handler register;
+    private final AuthFacade authFacade;
 
     LoginRestController(AuthFacade authFacade) {
-
-        login = ctx -> {
-            LoginRequestDTO loginDTO = ctx.bodyAsClass(LoginRequestDTO.class);
-            int id = loginDTO.id;
-            String password = loginDTO.password;
-            ResponseResolver.resolve(authFacade.login(id,password),ctx);
-        };
-
-        register = ctx -> {
-            RegisterRequestDTO registerDTO = ctx.bodyAsClass(RegisterRequestDTO.class);
-            String password = registerDTO.password;
-            ResponseResolver.resolve(authFacade.register(password),ctx);
-        };
+        this.authFacade = authFacade;
     }
 
     @Override
     public List<JavalinHandler> handlers() {
         return List.of(
-                JavalinHandler.post("/login", login),
-                JavalinHandler.post("/register", register)
+                JavalinHandler.post("/login", this::login),
+                JavalinHandler.post("/register", this::register)
         );
     }
+
+    private void login(Context ctx) {
+        LoginRequestDTO loginDTO = ctx.bodyAsClass(LoginRequestDTO.class);
+        int id = loginDTO.id;
+        String password = loginDTO.password;
+        ResponseResolver.resolve(authFacade.login(id, password), ctx);
+    }
+
+    private void register(Context ctx) {
+        RegisterRequestDTO registerDTO = ctx.bodyAsClass(RegisterRequestDTO.class);
+        String password = registerDTO.password;
+        ResponseResolver.resolve(authFacade.register(password), ctx);
+    }
+
 }
--- src/main/java/io/github/krasnoludkolo/user/UserRestController.java
+++ src/main/java/io/github/krasnoludkolo/user/UserRestController.java
@@ -3,25 +3,26 @@ package io.github.krasnoludkolo.user;
 import io.github.krasnoludkolo.infrastructure.http.Controller;
 import io.github.krasnoludkolo.infrastructure.http.JavalinHandler;
 import io.github.krasnoludkolo.infrastructure.http.ResponseResolver;
-import io.javalin.Handler;
+import io.javalin.Context;
 import io.vavr.collection.List;
 
 public final class UserRestController implements Controller {
 
-    private final Handler getUser;
+    private final UserFacade userFacade;
 
     UserRestController(UserFacade userFacade) {
-        getUser = ctx -> {
-            Integer id = ctx.pathParam("id", Integer.class).get();
-            ResponseResolver.resolve(userFacade.getUserInfo(id), ctx);
-        };
+        this.userFacade = userFacade;
     }
 
-    public List<JavalinHandler> handlers(){
+    public List<JavalinHandler> handlers() {
         return List.of(
-            JavalinHandler.get("/users/:id",getUser)
+                JavalinHandler.get("/users/:id", this::getUser)
         );
     }
 
+    private void getUser(Context ctx) {
+        Integer id = ctx.pathParam("id", Integer.class).get();
+        ResponseResolver.resolve(userFacade.getUserInfo(id), ctx);
+    }
 
 }
